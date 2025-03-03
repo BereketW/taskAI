8f4107a7f624fd65797ef940742438b100f6ddf6
BereketW <bereketsodenoo@gmail.com>
2025-03-03 22:51:07 +0300

    email: function created using resend

---
+++ actions/send-email.ts
@@ -0,0 +1,42 @@
+// import emailjs from "@emailjs/browser";
+
+// // Initialize EmailJS once
+// emailjs.init(process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY);
+
+// export async function email(data) {
+//   try {
+//     const response = await emailjs.send(
+//       process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID,
+//       process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID,
+//       {
+//         verification_url: data.verification_url,
+//       },
+//       process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY
+//     );
+
+//     return response; // Return success response
+//   } catch (error) {
+//     console.error("Email sending error:", error);
+//     throw error; // Re-throw for better error handling
+//   }
+// }
+
+// import EmailTemplate from "@/components/email-template";
+// import { renderToHTML } from "next/dist/server/render";
+// import nodemailer from "nodemailer";
+// import { render } from "react-dom";
+import { Resend } from "resend";
+const resend = new Resend("re_4ffmShNX_DunFtmzMHjcvKKanYv1qtadw");
+export async function email(value) {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "bereketsodeno7@gmail.com",
+      to: ["anonytempo5@gmail.com"],
+      subject: "TaskAI - Verify your email address",
+      html: `<div><p>Verify your email address</p><a href="http://localhost:3000">Verify</a></div>`,
+    });
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
