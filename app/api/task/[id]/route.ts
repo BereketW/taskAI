6b086d73469a18a8c1dcf71c97f7ae13ab23cd57
BereketW <bereketsodenoo@gmail.com>
2025-03-11 17:31:07 +0300

    implemented: single task fetch upon request

--- app/api/task/[id]/route.ts
+++ app/api/task/[id]/route.ts
@@ -1,4 +1,5 @@
 import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
@@ -9,7 +10,35 @@ export async function PATCH(
   const { id } = await params;
   const session = await auth.api.getSession({ headers: req.headers });
   if (!session) {
-    return NextResponse.status(401).json({ error: "Unauthorized" });
+    return NextResponse.json({ error: "Unauthorized" });
   }
   console.log(id);
+}
+export async function GET(
+  req: NextRequest,
+  res: NextResponse,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  console.log("params", params);
+  const id = (await params).id;
+  const session = await auth.api.getSession({ headers: req.headers });
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  try {
+    const task = await prisma.task.findUnique({
+      where: { id },
+      include: {
+        subtask: true,
+        assignedTo: true,
+      },
+    });
+    return NextResponse.json({ task: task }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Unable to fetch task" },
+      { status: 404 }
+    );
+  }
 }
