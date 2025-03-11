42517040fde6fa73839af8f155cda22e1718432f
BereketW <bereketsodenoo@gmail.com>
2025-03-11 17:31:55 +0300

    server side task detail implemented

--- app/(dashboard)/tasks/page.tsx
+++ app/(dashboard)/tasks/page.tsx
@@ -1,9 +1,10 @@
 import { fetchTask } from "@/actions/tasks";
 import TasksPage from "@/components/tasks/fetch-task";
 import React from "react";
+// import TasksPage from "../ai/smart-schedule/page";
 
 export default async function page() {
   const { tasks } = await fetchTask();
 
-  return <Tasks Page tasks={tasks} />;
+  return <TasksPage tasks={tasks} />;
 }
