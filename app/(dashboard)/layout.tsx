9256b86e3ce5ce763f121d84d9fa7609ce7c8cdf
BereketW <bereketsodenoo@gmail.com>
2025-03-13 16:21:51 +0300

    implemented: realtime notification frontend

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,6 +2,8 @@ import type React from "react";
 import { DashboardHeader } from "@/components/dashboard/header";
 import { AppSidebar } from "@/components/app-sidebar";
 import { ThemeProvider } from "next-themes";
+import { NotificationProvider } from "@/components/ui/notification-bar";
+import { NotificationDemo } from "@/components/notification-demo";
 // import { ThemeProvider } from "@/components/theme-provider";
 
 export default function RootLayout({
@@ -16,16 +18,18 @@ export default function RootLayout({
       enableSystem
       disableTransitionOnChange
     >
+      {/* <NotificationProvider> */}
       <div className="flex gap-4 py-6 px-4 ">
         <div className="">
           <AppSidebar />
         </div>
-
         <main className="flex-1">
           <DashboardHeader />
           {children}
         </main>
       </div>
+      {/* <NotificationDemo /> */}
+      {/* </NotificationProvider> */}
     </ThemeProvider>
   );
 }
