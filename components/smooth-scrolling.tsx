b2c0ea5ae5582fcba5807ad819de78c8b50f775f
BereketW <bereketsodenoo@gmail.com>
2025-04-23 22:18:08 +0300

    implemented lenish smooth scrolling behaviour

--- components/smooth-scrolling.tsx
+++ components/smooth-scrolling.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { ReactLenis } from "@studio-freight/react-lenis";
 export default function SmoothScrolling({ children }) {
   return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1, smoothWheel: true }}>
       {children}
     </ReactLenis>
   );
