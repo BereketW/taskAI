2bb3158c4e7927b3c968ea244f2368a3740a6c1f
BereketW <bereketsodenoo@gmail.com>
2025-04-23 22:19:50 +0300

    wrapped the children props with lenis smooth scroll

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,13 +4,17 @@ import { AppSidebar } from "@/components/app-sidebar";
 import { ThemeProvider } from "next-themes";
 import { NotificationProvider } from "@/components/ui/notification-bar";
 import { NotificationDemo } from "@/components/notification-demo";
+import { checkDeadline } from "@/lib/cron";
 // import { ThemeProvider } from "@/components/theme-provider";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // setInterval(async () => {
+  //   await checkDeadline();
+  // }, 1000);
   return (
     <ThemeProvider
       attribute="class"
