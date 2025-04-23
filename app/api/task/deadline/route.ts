7db0110d7619456f885ad7ebead60b1a050708f0
BereketW <bereketsodenoo@gmail.com>
2025-04-23 22:21:29 +0300

    made some api route handling changes

---
+++ app/api/task/deadline/route.ts
@@ -0,0 +1,28 @@
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const session = await auth.api.getSession({ headers: req.headers });
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized Access" }, { status: 401 });
+  }
+  try {
+    const tasks = await prisma.task.findMany({
+      where: {
+        isCompleted: false,
+        status: "PENDING",
+        dueDate: {
+          lte: new Date().toISOString(),
+        },
+      },
+    });
+    console.log("Deadline Notification", tasks);
+    if (tasks.length > 0) {
+      return NextResponse.json({ response: tasks, status: "success" });
+    }
+    return NextResponse.json({ response: tasks, status: "error" });
+  } catch (error) {
+    console.log(error);
+  }
+}
