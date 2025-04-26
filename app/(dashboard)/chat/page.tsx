8af4840cf00c9bd61ddb900c66b57ba34cbf4f86
BereketW <bereketsodenoo@gmail.com>
2025-04-26 17:17:36 +0300

    made ui changes

--- app/(dashboard)/chat/page.tsx
+++ app/(dashboard)/chat/page.tsx
@@ -1,18 +1,10 @@
-// import { Chat } from "@/components/chat";
-
 import Chat from "@/components/chat";
 
-// import { Chat } from "@/components/chat";
-
-export default async function Home() {
+export default function Home() {
   return (
-    <main className="flex  flex-col items-center  bg-background p-4  dark hero-gradient">
-      <div className="relative w-full max-w-4xl">
-        <div className="absolute -top-20 -left-20 dashboard-decoration decoration-1 opacity-60"></div>
-        <div className="absolute -bottom-20 -right-20 dashboard-decoration decoration-2 opacity-60"></div>
-        <div className="card-elegant backdrop-blur-sm">
-          <Chat />
-        </div>
+    <main className="flex flex-col items-center justify-center bg-background p-0 h-[90vh] overflow-hidden">
+      <div className="w-[60svw] max-sm:w-full h-full">
+        <Chat />
       </div>
     </main>
   );
